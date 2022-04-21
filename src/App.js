bdd3eff40fcbce657266d20a02016cb473aa515d
Rahul-Verma <94426476+rvstarcaptain@users.noreply.github.com>
2022-04-21 23:58:34 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
+import {Routes,Route} from "react-router-dom"
+
+
+
+import Nav from "./Component/Nav";
+import ProductList from "./Component/ProductList";
 
 
 function App() {
-
+  
   return(
     <div>
-      
+      <Nav/>
+      <Routes>
+            <Route  exact path="/category/top-wear-kurtas" element={<ProductList key="top-wear-kurtas" name="top-wear-kurtas" /> }/>
+            <Route  exact path="/category/bottom-wear" element={<ProductList key="bottom-wear" name="bottom-wear" /> }/>
+            <Route  exact path="/category/footwear" element={<ProductList key="footwear" name="footwear" /> }/>
+            <Route  exact path="/category/w-cosmetics" element={<ProductList key="w-cosmetics" name="w-cosmetics" /> }/>
+            <Route  exact path="/category/drapes-wear" element={<ProductList key="drapes-wear" name="drapes-wear" /> }/>
+      </Routes>
       
     </div>
   )
