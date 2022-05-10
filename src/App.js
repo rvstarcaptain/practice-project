b41694cde58906b0c0dbe022ff5d7f4d3089bc03
Rahul-Verma <94426476+rvstarcaptain@users.noreply.github.com>
2022-05-10 17:44:19 +0530

    commit -m "todo&calc"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 
-import {Routes,Route} from "react-router-dom"
-
-
-
+import {Routes,Route} from "react-router-dom";
 import Nav from "./Component/Nav";
 import ProductList from "./Component/ProductList";
+import Todolist1 from "./Component/Todolist1";
+import Calculator from "./Component/Calculator";
 
 
 function App() {
   
   return(
     <div>
-      <Nav/>
+      {/* <Nav/>
       <Routes>
             <Route  exact path="/category/top-wear-kurtas" element={<ProductList key="top-wear-kurtas" name="top-wear-kurtas" /> }/>
             <Route  exact path="/category/bottom-wear" element={<ProductList key="bottom-wear" name="bottom-wear" /> }/>
             <Route  exact path="/category/footwear" element={<ProductList key="footwear" name="footwear" /> }/>
             <Route  exact path="/category/w-cosmetics" element={<ProductList key="w-cosmetics" name="w-cosmetics" /> }/>
             <Route  exact path="/category/drapes-wear" element={<ProductList key="drapes-wear" name="drapes-wear" /> }/>
-      </Routes>
+      </Routes> */}
       
+      <Todolist1/>
+      {/* <Calculator/> */}
     </div>
   )
   
